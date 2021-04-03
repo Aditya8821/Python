7f4fb3ded35bf36b64ed093984e52fb948cd7050
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-04-04 01:36:28 +0530

    Delete 2.4.py

--- Python/2.4.py
+++
@@ -1,8 +0,0 @@
-"""Wap to find greatest among 3 number"""
-x,y,z=int(input("1st No:")),int(input("2nd No:")),int(input("3rd No:"))
-if (x>y) and (x>y):
-    print(x)
-elif (y>x) and (y>z):
-    print(y)
-elif (z>x) and (z>y):
-    print(z)
