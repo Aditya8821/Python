d1d5d8df493523e0011aca112dd85b037ab41ffe
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-04-04 01:37:26 +0530

    Delete 2.7.py

--- Python/2.7.py
+++
@@ -1,9 +0,0 @@
-a,b,c=int(input("a=")),int(input("b=")),int(input("c="))
-d=b*b-4*a*c
-if d>0:
-    print("Roots are Real and Different")
-elif d==0:
-    print("Roots are Real and Same")
-elif d<0:
-    print("Roots are Complex")
-    
