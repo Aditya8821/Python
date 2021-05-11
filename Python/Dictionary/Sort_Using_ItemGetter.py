684f4c8ccf4870086aebfe8b881d33967578d40f
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-12 00:02:28 +0530

    Delete Sort_Using_ItemGetter.py

--- Python/Dictionary/Sort_Using_ItemGetter.py
+++
@@ -1,8 +0,0 @@
-from operator import itemgetter
-lis = [{ "name" : "Nandini", "age" : 20}, { "name" : "Manjeet", "age" : 20 },{ "name" : "Nikhil" , "age" : 19 }]
-res=sorted(lis,key=itemgetter('age'))
-print("Sorted by age:"+str(res))
-res=sorted(lis,key=itemgetter('age','name'))
-print("Sorted by name and age:"+str(res))
-res=sorted(lis,key=itemgetter('age'),reverse=True)
-print("Ascending order of age:"+str(res))
