48b366ec3ffbc1ac8d65d0dfa8f704362e7ebca1
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-11 23:56:42 +0530

    Delete Finding_Duplicate.py

--- Python/String/Finding_Duplicate.py
+++
@@ -1,9 +0,0 @@
-def duplicate(string):
-    dup_list=[]
-    for str in string:
-        if string.count(str) >1 and str not in dup_list:
-            print(str)
-            dup_list.append(str)
-        
-string="geeksforgeeks"
-duplicate(string)
