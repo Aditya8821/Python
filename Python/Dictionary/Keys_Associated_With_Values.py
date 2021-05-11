5f11b1463c69ea07cdc18267e7fd0cc8abaf6d40
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-12 00:01:33 +0530

    Delete Keys_Associated_With_Values.py

--- Python/Dictionary/Keys_Associated_With_Values.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-test_dict = {'gfg' : [1, 2, 3], 'is' : [1, 4], 'best' : [4, 2]}
-res=defaultdict(list)
-for key,value in test_dict.items():
-    for ele in value:
-        res[ele].append(key)
-print(dict(res))
-        
