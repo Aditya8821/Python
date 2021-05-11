d0dabb417c949086fe77a1f994140b98857c3751
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-11 23:47:05 +0530

    Delete Join_Tuples_if_similar_initial_element.py

--- Python/Tuple/Join_Tuples_if_similar_initial_element.py
+++
@@ -1,9 +0,0 @@
-test_list = [(5, 6), (5, 7), (6, 8), (6, 10), (7, 13)]
-res=[]
-for i in test_list:
-    if res and res[-1][0]==i[0]:
-        res[-1].extend(i[1:])
-    else:
-        res.append([ele for ele in i])
-res=list(map(tuple,res))
-print(res)
