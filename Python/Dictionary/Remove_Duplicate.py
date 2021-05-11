a7fe14c8a33f771afe8267e28e897013a25ee3a3
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-12 00:02:35 +0530

    Delete Remove_Duplicate.py

--- Python/Dictionary/Remove_Duplicate.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-def remDup(input):
-    input=input.split(" ")
-    Uniq=Counter(input)
-    res=" ".join(Uniq.keys())
-    print(res)
-input = 'Python is great and Java is also great'
-remDup(input)
