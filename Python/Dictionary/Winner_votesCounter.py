5757ed2dbeab2caf1c11df67801cf7480d09eb99
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-12 00:02:58 +0530

    Delete Winner_votesCounter.py

--- Python/Dictionary/Winner_votesCounter.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-def winner(votes):
-    vote_count=Counter(votes)
-    max_votes=max(vote_count.values())
-    lst=[i for i in vote_count.keys() if vote_count[i]==max_votes]
-    print(sorted(lst[0]))
-votes =['john','johnny','jackie','johnny','john','jackie','jamie','jamie','john','johnny','jamie','johnny','john'] 
-winner(votes) 
