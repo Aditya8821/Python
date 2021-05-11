28f6d450e855cd41aea6b6e26a939a609d884c59
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-12 00:01:01 +0530

    Delete Convert_List_Tuples.py

--- Python/Dictionary/Convert_List_Tuples.py
+++
@@ -1,7 +0,0 @@
-def convert(tup,dict):
-    for a,b in tup:
-        dict[a]=[b]
-    return dict
-tups = [("akash", 10), ("gaurav", 12), ("anand", 14), ("suraj", 20), ("akhil", 25), ("ashish", 30)]
-dict={}
-print(convert(tups,dict))
