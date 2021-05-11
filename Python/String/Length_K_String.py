3bc347e6478c9e6c4fcd3f2b0b235192b57d78a2
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-11 23:56:59 +0530

    Delete Length_K_String.py

--- Python/String/Length_K_String.py
+++
@@ -1,8 +0,0 @@
-def string_k(k,sentence):
-    str=sentence.split(" ")
-    res=[]
-    res=[word for word in str if len(word)>k]
-    return res
-k = 3
-sentence ="geek for geeks"
-print(string_k(k,sentence))
