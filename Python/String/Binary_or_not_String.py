83adf9cd18b5a22c9cbaa5fe76be1139bb2e147b
Aditya Trivedi <62375127+Aditya8821@users.noreply.github.com>
2021-05-11 23:56:16 +0530

    Delete Binary_or_not_String.py

--- Python/String/Binary_or_not_String.py
+++
@@ -1,8 +0,0 @@
-def binary_or_not(str):
-    binary="01"
-    return all([num in binary for num in str])
-str="001021010001010"
-if binary_or_not(str):
-    print("Binary")
-else:
-    print("Not Binary")
