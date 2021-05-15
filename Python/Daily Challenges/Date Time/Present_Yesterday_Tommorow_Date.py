71c7d90b085796acbe1053217b563dc34afb6946
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2021-05-16 00:20:49 +0530

    Add files via upload

---
+++ Python/Daily Challenges/Date Time/Present_Yesterday_Tommorow_Date.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+presentday=datetime.now()
+yesterday=presentday-timedelta(1)
+tomorrow=presentday+timedelta(1)
+print("Yesterday = ", yesterday.strftime('%d-%m-%Y'))
+print("Today = ", presentday.strftime('%d-%m-%Y'))
+print("Tomorrow = ", tomorrow.strftime('%d-%m-%Y'))
