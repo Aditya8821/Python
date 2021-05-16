dec9b032f29b7e0495b3f4b44b8d4db338cedb90
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2021-05-17 01:31:18 +0530

    Delete Check_Defined_Characters.py

--- Python/Daily Challenges/Regex/Check_Defined_Characters.py
+++
@@ -1,9 +0,0 @@
-import re
-def check(str,pattern):
-    if re.search(pattern,str):
-        print("Valid String")
-    else:
-        print("InValid String")
-pattern=re.compile('^[1234]+$')
-check('2134',pattern)
-check('349',pattern)
